866d6222fd86c513b4e4d62f082adb2d0e2f9a2a
Ondrej Valenta <ondrej.valenta@keenmate.com>
2020-05-16 15:29:58 +0200

    Vuepress config file

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: 'Github Pages Test',
+  base: '/github-pages-test/',
+  description: 'Simple yet powerful Github Pages in Vuepress',
+  themeConfig: {
+    repo: 'keenmate/github-pages-test',
+    docsDir: 'docs',
+    editLinks: true,
+    sidebarDepth: 1,
+    sidebar: [
+      '/'
+    ]
+  }
+}
