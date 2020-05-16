d612ea4b30e18a20840ad91492a101922855e172
Ondrej Valenta <ondrej.valenta@keenmate.com>
2020-05-16 16:17:40 +0200

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  title: 'Github Pages Test',
-  base: '/github-pages-test/',
+  title: 'Vuepress Github Pages',
+  base: '/vuepress-github-pages/',
   description: 'Simple yet powerful Github Pages in Vuepress',
   themeConfig: {
-    repo: 'keenmate/github-pages-test',
+    repo: 'keenmate/vuepress-github-pages',
     docsDir: 'docs',
     editLinks: true,
     sidebarDepth: 1,
